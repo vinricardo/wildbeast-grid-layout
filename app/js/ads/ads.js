2aac4fb0ba1d7bb109f55ac27b3b29548da4e618
Vinicius <vinicius.01deagosto@gmail.com>
2022-06-04 19:22:54 -0300

    feature: finalizando estruturação html e reduzindo atráves de arquivos js

---
+++ app/js/ads/ads.js
@@ -0,0 +1,11 @@
+{
+  var element = document.getElementById("ads");
+
+  element.innerHTML = `
+    <div class="ad-item">
+        <img src="img/anuncio-1.jpg" alt="Ad 1" />
+    </div>
+    <div class="ad-item">
+        <img src="img/anuncio-2.jpg" alt="Ad 2" />
+    </div>`;
+}
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,4 +1,16 @@
 {
+  var element = document.getElementById("ads");
+
+  element.innerHTML = `
+    <div class="ad-item">
+        <img src="img/anuncio-1.jpg" alt="Ad 1" />
+    </div>
+    <div class="ad-item">
+        <img src="img/anuncio-2.jpg" alt="Ad 2" />
+    </div>`;
+}
+
+{
   var element = document.getElementById("header");
 
   element.innerHTML = `
